e2196f104d5c8362da02c361c261457db659c2c3
dmjendor@gmail.com <dmjendor@gmail.com>
2025-12-04 11:17:19 -0500

    Create ticket components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboards/server-status/server-status.component';
 import { TrafficComponent } from './dashboards/traffic/traffic.component';
 import { TicketsComponent } from './dashboards/tickets/tickets.component';
+import { DashboardItemComponent } from './dashboards/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { TicketsComponent } from './dashboards/tickets/tickets.component';
     ServerStatusComponent,
     TrafficComponent,
     TicketsComponent,
+    DashboardItemComponent,
   ],
 })
 export class AppComponent {}
