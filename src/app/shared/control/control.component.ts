d0b0ffe0508d97363441fce3669a39b58f8d117e
dmjendor@gmail.com <dmjendor@gmail.com>
2025-12-04 12:36:42 -0500

    Interacting with host lements from inside components

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -7,6 +7,7 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  host: { class: 'control' }, // adds key/value pairs as attributes to the host element
 })
 export class ControlComponent {
   label = input.required<string>();
